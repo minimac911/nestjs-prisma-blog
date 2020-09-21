035fc5d2507a0d450db1ce11af718f23fba23e1f
Thomas McAlpine <thomasmcalpine77@gmail.com>
2020-09-21 11:06:04 +0200

    Init

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserService } from "./user/user.service";
+import { PostService } from "./post/post.service";
+import { PrismaService } from "./shared/services/prisma.service";
+
+@Module({
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService, UserService, PostService, PrismaService],
+})
+export class AppModule { }
---
+++ src/shared/base.controller.ts
@@ -0,0 +1,13 @@
+
+// import { SECRET } from '../config';
+// import * as jwt from 'jsonwebtoken';
+
+// export class BaseController {
+//     protected getUserIdFromToken(auth){
+//         if(!auth) return null;
+
+//         const token = authorization.split(' ')[1];
+//         const decoded: any = jwt.verify(token, SECRET);
+//         return decoded.id;
+//     }
+// }
